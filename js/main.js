185b5f30932924851d78f47604a4397032e899c0
MC SOPORTE <mcsoport@cl.ibm.com>
2020-06-04 12:58:10 -0400

    v1.0.0

---
+++ js/main.js
@@ -0,0 +1,64 @@
+var url = 'https://comunidad.canal108.cl/';
+
+window.addEventListener("load", function(){
+	
+	$('.btn-like').css('cursor', 'pointer');
+	$('.btn-dislike').css('cursor', 'pointer');
+
+	// Botón de like
+	function like(){
+		$('.btn-like').unbind('click').click(function(){
+			console.log('like');
+			$(this).addClass('btn-dislike').removeClass('btn-like');
+			$(this).attr('src', url+'img/heart-red.png');
+
+			$.ajax({
+				url: url+'like/'+$(this).data('id'),
+				type: 'GET',
+				success: function(response){
+					if(response.like){
+						console.log('Has dado like a la publicación');
+					}else{
+						console.log('Error al dar like');
+					}
+				}
+			});
+
+			dislike();
+		})
+	}
+
+	like();
+
+	// Botón de dislike
+	function dislike(){
+		$('.btn-dislike').unbind('click').click(function(){
+			console.log('dislike');
+			$(this).addClass('btn-like').removeClass('btn-dislike');
+			$(this).attr('src', url+'img/heart-black.png');
+
+			$.ajax({
+				url: url+'dislike/'+$(this).data('id'),
+				type: 'GET',
+				success: function(response){
+					if(response.like){
+						console.log('Has dado dislike a la publicación');
+					}else{
+						console.log('Error al dar dislike');
+					}
+				}
+			});
+
+			like();
+		});
+	}
+
+	dislike();
+
+	// BUSCADOR
+	$('#buscador').submit(function(e){
+		$(this).attr('action',url+'/buscar/'+$('#buscador #search').val());
+	});
+
+
+});
